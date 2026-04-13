3cd4b11a3a43efa60e8255834666b5c44ef1bfa9
Erison Felipe <erisonfelipe1@outlook.com>
2026-04-13 13:33:58 -0300

    Atualização nos arquivos: db.js/index.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require("pg");
+const { Pool } = require("pg")
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "costura_db",
-  password: process.env.DB_PASSWORD,
-  port: 5433,
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
-module.exports = pool;
+module.exports = pool
